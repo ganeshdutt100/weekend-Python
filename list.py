861f507265e7fecc89314ff894daa60ab5d170d4
unknown <gdutt2304@gmail.com>
2026-05-17 13:10:39 +0530

    update list

---
+++ list.py
@@ -0,0 +1,7 @@
+# listValue  = ["apple","banana","orange","apple"]
+# listValue[-3] = "mango"
+# listValue.append("cherry")
+# print(listValue[1:3])
+# print(listValue[:3])
+# print(listValue[0:])
+# print(listValue[::-1])
