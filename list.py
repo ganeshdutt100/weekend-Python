94e13d03b811c730e1757d88a83bc1f27c9d4e93
unknown <gdutt2304@gmail.com>
2026-05-17 13:31:09 +0530

    update list

--- list.py
+++ list.py
@@ -1,6 +1,30 @@
-# listValue  = ["apple","banana","orange","apple"]
-# listValue[-3] = "mango"
+# listValue  = ["apple","banana","orange"]
+# list2=[1,2,3,4,5,6,78]
 # listValue.append("cherry")
+# listValue.insert(2,"kiwi")
+# listValue.insert(1,"Python")
+# listValue.extend(list2)
+
+# print(listValue.remove("apple"))
+# result = listValue.remove()
+# result = listValue.pop()
+# result = listValue.clear()
+
+listValue  = [1,4,7,3,3,1,2,9]
+# result = listValue.sort()
+# result = listValue.reverse()
+# result = listValue.index(7)
+# result = listValue.count(3)
+
+# print(listValue)
+for i in listValue:
+    print(i*2)
+# print(result)
+# print()
+
+
+# print(listValue)
+# listValue[-3] = "mango"
 # print(listValue[1:3])
 # print(listValue[:3])
 # print(listValue[0:])
