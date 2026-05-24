c87f42c0473b1d0e17b38bf1b9a32e06d3c58760
unknown <gdutt2304@gmail.com>
2026-05-24 12:36:32 +0530

    List

---
+++ Dictionary.py
@@ -0,0 +1,29 @@
+# my_dict =  {
+#     # "key":"value",
+#     "name":"Anand",
+#     "age":30,
+#     "city":"Delhi"
+# }
+# print(my_dict["age"])
+
+# set 
+# my_set = {1,23,45,1,678,1,1,98,76,5,43}
+# print(my_set)
+
+# list 
+name  = ["Anand","Rahul","Anand","Priya","Anjali"]
+# name.append("js")
+# name.insert(2, "Sneha")
+# name.extend(["Rohit","Suman"])
+# name.remove("Rahul")
+# name.pop()
+# name.clear()
+# name.reverse()
+name.sort()
+
+
+# print(name.index("Priya"))
+print(name.count("Anand"))
+for x in name:
+    print(x)
+
